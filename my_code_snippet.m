34284cc4803c22de7f01928adc1e673a0774dbc0
FrankZhang <mynameiszhangyong@gmail.com>
2015-09-23 13:05:54 +0800

    Imported user code snippets

---
+++ my_code_snippet.m
@@ -0,0 +1,11 @@
+// My Code Snippet
+// 
+//
+// IDECodeSnippetCompletionPrefix: pra
+// IDECodeSnippetCompletionScopes: [All]
+// IDECodeSnippetIdentifier: 1C7D9B90-0582-4820-B758-05DCA57F776F
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+#pragma
+#pragma mark -<#statements#>
